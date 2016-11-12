f9d7535f4cd2498c96b5cb3c2c7b367f1d225662
Zhiganoff <zhiganoff.rd@yandex.ru>
2016-11-13 01:02:06 +0300

    Created working

---
+++ practice/4-shell/working.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <cstring>
+#include <stdexcept>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+static const std::string str_delims(" ><&|");
+static std::set<char> delims;
+static std::set<std::string> operators;
+static std::set<std::string> expr_operators;
+
+void fill_sets() {
+    for (const char& c : str_delims) {
+        delims.insert(c);
+    }
+    operators.insert("|");
+    operators.insert("&&");
+    operators.insert("||");
+    operators.insert(">");
+    operators.insert("<");
+    operators.insert("&");
+    expr_operators.insert("|");
+    expr_operators.insert("&&");
+    expr_operators.insert("||");
+    expr_operators.insert("&");
+}
+
+struct Expression {
+    Expression(const std::vector<std::string>& input);
+    Expression(std::string token, Expression a) : token(token), args{ a } {}
+    Expression(std::string token, Expression a, Expression b) : token(token), args{ a, b } {}
+
+    void dump();
+    int execute();
+    // static void conveyor(Command* cmd);
+
+    char** argv;         // список из имени команды и аргументов
+    uint argc;           // количество аргументов
+    std::string infile;  // переназначенный файл стандартного ввода
+    std::string outfile; // переназначенный файл стандартного вывода
+    bool backgrnd;       // True, если команда подлежит выполнению в фоновом режиме
+
+    std::string token;
+    std::vector<Expression> args;
+};
+
+Expression::Expression(const std::vector<std::string>& input) {
+    argv = nullptr;
+    argc = 0;
+    backgrnd = false;
+    auto it = input.begin();
+    while (it != input.cend() && operators.find(*it) == operators.cend()) {
+        argv = (char**)realloc(argv, (argc + 1) * sizeof(char *));
+        argv[argc++] = strdup(it->c_str());
+        it++;
+    }
+    argv = (char**)realloc(argv, (argc + 1) * sizeof(char *));
+    argv[argc] = nullptr;
+    while (it != input.cend()) {
+        if (!it->compare("<")) {
+            infile = *(++it);
+            it++;
+        } else if (!it->compare(">")) {
+            outfile = *(++it);
+            it++;
+        } else if (!it->compare("&")) {
+            backgrnd = true;
+            it++;
+        }
+    }
+}
+
+void Expression::dump() {
+    std::cout << "debug: Entered dump()" << std::endl;
+    if (args.size()) {
+        std::cout << token << ' ' << args.size() << std::endl;
+    }
+    switch (args.size()) {
+        case 2: {
+            args[0].dump();
+            std::cout << token << std::endl;
+            args[1].dump();
+            // std::cout << "debug wtf1 " << args.size() << std::endl;
+            return;
+        }
+        case 0:
+            std::cout << "argv: ";
+            for (uint idx = 0; idx < argc; idx++) {
+                std::cout << argv[idx] << ' ';
+            }
+            std::cout << std::endl;
+            if (infile.size()) {
+                std::cout << "infile: " << infile << std::endl;
+            }
+            if (outfile.size()) {
+                std::cout << "outfile: " << outfile << std::endl;
+            }
+            std::cout << "background: " << backgrnd << std::endl;
+        }
+    // std::cout << "debug wtf" << std::endl;
+}
+
+int Expression::execute() {
+    std::cout << "debug: Entered execute()" << std::endl;
+    if (args.size()) {
+        std::cout << token << ' ' << args.size() << std::endl;
+    }
+    switch (args.size()) {
+        case 2: {
+            int ex_status = args[0].execute();
+            // std::cout << token << std::endl;
+            if (!token.compare("&&"))
+            {
+                // std::cout << ex_status << std::endl;
+                if (WIFEXITED(ex_status)) {
+                    if (!WEXITSTATUS(ex_status)) {
+                        return args[1].execute();
+                    }
+                }
+            } else {
+                // std::cout << ex_status << std::endl;
+                if (!WIFEXITED(ex_status) || (WIFEXITED(ex_status) && WEXITSTATUS(ex_status))) {
+                    return args[1].execute();
+                }
+            }
+            // std::cout << ex_status << std::endl;
+            return ex_status;
+        }
+        case 0: {
+            pid_t pid;
+            pid = fork();
+            if (pid == 0) {
+                /* Перенаправление ввода/вывода */
+                int fdIn, fdOut;
+                if (infile.size()) {
+                    if ((fdIn = open(infile.c_str(), O_RDONLY, 0666)) == -1) {
+                        std::cout << "Can't open " << infile << std::endl;
+                        return 0;
+                        }
+                    dup2(fdIn, STDIN_FILENO);
+                    close(fdIn);
+                }
+                if (outfile.size()) {
+                    fdOut = open(outfile.c_str(),  O_CREAT | O_TRUNC | O_WRONLY, 0666);
+                    dup2(fdOut, STDOUT_FILENO);
+                    close(fdOut);
+                }
+                if (argv) {
+                    execvp(argv[0], argv);
+                    perror(argv[0]);
+                    exit(1);
+                }
+            }
+            int ex_status;
+            wait(&ex_status);
+            // std::cout << ex_status << std::endl;
+            return ex_status;
+        }
+        default:
+            return 1;
+    }
+}
+
+class Parser {
+public:
+    explicit Parser(std::string& input);
+    Expression parse();
+private:
+    std::string parse_token();
+    Expression parse_simple_expression();
+    Expression parse_binary_expression();
+
+    std::vector<std::string> tokens;
+};
+
+Parser::Parser(std::string& input) {
+    tokens.clear();
+    size_t idx = 0, len = 0;
+    while (input[idx] == ' ') {
+        idx++;
+    }
+    while (idx != input.size()) {
+        if (delims.find(input[idx]) == delims.cend()) {
+            len = 1;
+            while (idx + len != input.size() && delims.find(input[idx + len]) == delims.cend()){
+                len++;
+            }
+        } else {
+            len = 1;
+            if (delims.find(input[idx + 1]) != delims.cend() && input[idx + 1] != ' ') {
+                len++;
+            }
+        }
+        tokens.push_back(input.substr(idx, len));
+        idx += len;
+        while (input[idx] == ' ') {
+            idx++;
+        }
+    }
+}
+
+Expression Parser::parse_simple_expression() {
+    auto it = tokens.begin();
+    while (it != tokens.cend() && expr_operators.find(*it) == expr_operators.cend()) {
+        it++;
+    }
+    std::vector<std::string> argv(tokens.begin(), it);
+    // std::cout << "debug7" << std::endl;
+    // for (const auto& t : argv) {
+    //     std::cout << t << ' ';
+    // }
+    // std::cout << std::endl;
+    tokens.erase(tokens.begin(), it);
+    return Expression(argv);
+}
+
+Expression Parser::parse_binary_expression() {
+    // std::cout << "debug5" << std::endl;
+    Expression left_expr = parse_simple_expression();
+    // std::cout << "debug " << tokens.size() << std::endl;
+    // left_expr.dump();
+    while (tokens.size()) {
+        std::string op = tokens[0];
+        tokens.erase(tokens.begin());
+        // std::cout << "dubug op: " << op << std::endl;
+        // std::cout << "debug tokens.size(): " << tokens.size() << std::endl;
+
+        Expression right_expr = parse_binary_expression();
+        left_expr = Expression(op, left_expr, right_expr);
+    }
+    // std::cout << "debug7" << std::endl;
+    return left_expr;
+}
+
+Expression Parser::parse() {
+    return parse_binary_expression();
+}
+
+int main()
+{
+    fill_sets();
+    std::string input;
+    std::vector<std::string> tokens;
+    while (std::getline(std::cin, input)) {
+        // std::cout << "debug1" << std::endl;
+        Parser p(input);
+        // std::cout << "debug2" << std::endl;
+        Expression e = p.parse();
+        // std::cout << "debug3" << std::endl;
+        e.dump();
+        // std::cout << "debug: handled" << std::endl;
+        e.execute();
+    }
+    return 0;
+}
